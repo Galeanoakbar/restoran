bdd71aaa041801dd7353206b20c30d58f55bbe96
unknown <galeakbarakbar@gmail.com>
2025-01-16 13:40:31 +0700

    merubah tampilan

--- entri_referensi.php
+++ entri_referensi.php
@@ -148,47 +148,47 @@ if (isset($_SESSION['username'])) {
         }
     </style>
 </head>
+<body>                                               bbbbbbb
 <body>
-<body>
-  <button class="toggle-btn" onclick="toggleSidebar()">☰ Menu</button>
-  <div class="sidebar" id="sidebar">
-      <h3>Welcome, <?php echo htmlspecialchars($nama_user); ?></h3>
-      <ul>
-    <?php
-    if ($user['id_level'] == 1) { // Owner
-    ?>
-        <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
-        <a href="entri_referensi.php"><i class="fas fa-utensils"></i> Entri Referensi</a>
-        <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
-        <a href="entri_transaksi.php"><i class="fas fa-money-bill"></i> Entri Transaksi</a>
-        <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
-    <?php
-    } elseif ($user['id_level'] == 2) { // Level 2
-    ?>
-        <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
-        <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
-        <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
-    <?php
-    } elseif ($user['id_level'] == 3) { // Level 3
-    ?>
-        <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
-        <a href="entri_transaksi.php"><i class="fas fa-money-bill"></i> Entri Transaksi</a>
-        <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
-    <?php
-    } elseif ($user['id_level'] == 4) { // Level 4
-    ?>
-        <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
-        <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
-    <?php
-    } elseif ($user['id_level'] == 5) { // Level 5
-    ?>
-        <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
-        <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
-    <?php
-    }
-    ?>
-    <a href="logout.php" class="btn btn-danger w-100 mt-3"><i class="fas fa-sign-out-alt"></i> Logout</a>
-</ul>
+    <button class="toggle-btn" onclick="toggleSidebar()">☰ Menu</button>
+    <div class="sidebar" id="sidebar">
+        <h3>Welcome, <?php echo htmlspecialchars($nama_user); ?></h3>
+        <ul>
+        <?php
+        if ($user['id_level'] == 1) { // Owner
+        ?>
+            <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
+            <a href="entri_referensi.php"><i class="fas fa-utensils"></i> Entri Referensi</a>
+            <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
+            <a href="entri_transaksi.php"><i class="fas fa-money-bill"></i> Entri Transaksi</a>
+            <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
+        <?php
+        } elseif ($user['id_level'] == 2) { // Level 2
+        ?>
+            <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
+            <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
+            <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
+        <?php
+        } elseif ($user['id_level'] == 3) { // Level 3
+        ?>
+            <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
+            <a href="entri_transaksi.php"><i class="fas fa-money-bill"></i> Entri Transaksi</a>
+            <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
+        <?php
+        } elseif ($user['id_level'] == 4) { // Level 4
+        ?>
+            <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
+            <a href="generate_laporan.php"><i class="fas fa-print"></i> Generate Laporan</a>
+        <?php
+        } elseif ($user['id_level'] == 5) { // Level 5
+        ?>
+            <a href="beranda.php"><i class="fas fa-home"></i> Beranda</a>
+            <a href="entri_order.php"><i class="fas fa-shopping-cart"></i> Entri Order</a>
+        <?php
+        }
+        ?>
+        <a href="logout.php" class="btn btn-danger w-100 mt-3"><i class="fas fa-sign-out-alt"></i> Logout</a>
+    </ul>
 
   </div>
     <!-- Content -->
